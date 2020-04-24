2f0ceb5a6071452d3c85cca606c74975857ecb86
Shikhor Kumer Roy <shikhor.kumer@bjitgroup.com>
2020-04-24 20:25:32 +0600

    Move wrongly added cockpit component from persons to app component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Cockpit from "../Cockpit/Cockpit";
 
 const Persons = () => {
     return (
         <div className="Persons">
-            <Cockpit></Cockpit>
         </div>
     );
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <h1> {this.props.title} </h1>
+                <Cockpit></Cockpit>
                 <Persons></Persons>
             </div>
         );
